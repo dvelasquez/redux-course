b00c39385ae643f8b8f0464444d958ed52c5d6ea
Danilo Velasquez Urrutia <danilo.velasquez@gmail.com>
2017-11-18 19:27:58 -0300

    Curso React + Redux de Egghead.io

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React with Redux Course</h1>
+                </header>
+                <div className="Todo-App">
+                    <TodoForm
+                        currentTodo={this.props.currentTodo}
+                        changeCurrent={this.props.changeCurrent}
+                    />
+                    <TodoList todos={this.props.todos}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/reducers/todo.js
@@ -0,0 +1,25 @@
+const initState = {
+    todos: [
+        {id: 1, name: 'Create a store', isComplete: true},
+        {id: 2, name: 'Load state through the store', isComplete: true},
+        {id: 3, name: 'Handle state changes with redux', isComplete: false}
+    ],
+    currentTodo: 'temp'
+};
+
+const CURRENT_UPDATE = 'CURRENT_UPDATE';
+const TODO_ADD = 'TODO_ADD';
+
+export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val});
+
+export default (state = initState, action) => {
+    switch (action.type) {
+        case TODO_ADD:
+            return {...state, todos: state.todos.concat(action.payload)};
+        case CURRENT_UPDATE:
+            return {...state, currentTodo: action.payload};
+        default:
+            return state;
+    }
+
+}
